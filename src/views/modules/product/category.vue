8a4315978c8df6e59795cf10437d85f02eaac8cb
xyzgithome <2584118953@qq.com>
2023-07-09 22:20:02 +0800

    54、商品服务-API-三级分类-修改-拖拽效果

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -7,6 +7,8 @@
       show-checkbox
       node-key="catId"
       :default-expanded-keys="expandeKey"
+      draggable
+      :allow-drop="allowDrop"
     >
       <span class="custom-tree-node" slot-scope="{ node, data }">
         <span>{{ node.label }}</span>
@@ -25,19 +27,36 @@
             @click="() => remove(node, data)"
             >Delete</el-button
           >
+          <el-button type="text" size="mini" @click="() => edit(data)"
+            >Edit</el-button
+          >
         </span>
       </span>
     </el-tree>
 
-    <el-dialog title="提示" :visible.sync="dialogVisible" width="30%">
+    <el-dialog
+      :title="title"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :close-on-click-modal="false"
+    >
       <el-form :model="category">
         <el-form-item label="分类名称">
           <el-input v-model="category.name" autocomplete="off"></el-input>
         </el-form-item>
+        <el-form-item label="图标">
+          <el-input v-model="category.icon" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="计量单位">
+          <el-input
+            v-model="category.productUnit"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="addCategory">确 定</el-button>
+        <el-button type="primary" @click="submitData">确 定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -53,7 +72,12 @@ export default {
   props: {},
   data () {
     return {
-      category: { name: '', parentCid: 0, catLevel: 0, showStatus: 1, sort: 0 },
+      maxLevel: 0,
+      title: '',
+      dialogType: '',
+      category: {
+        name: '', parentCid: 0, catLevel: 0, showStatus: 1, sort: 0, catId: null, productUnit: '', icon: ''
+      },
       dialogVisible: false,
       menus: [],
       expandeKey: [],
@@ -70,6 +94,65 @@ export default {
   watch: {},
   // 方法集合
   methods: {
+    allowDrop (draggingNode, dropNode, type) {
+      // 被拖动的当前节点以及所在的父节点总层数不能大于3
+
+      // 被拖动的当前节点的总层数
+      console.log('allowDrop', draggingNode, dropNode, type)
+
+      this.calculateDraggingNodeMaxLevel(draggingNode.data)
+
+      // 当前被拖拽的节点以及其子节点的总数目
+      let draggingNodeCount = this.maxLevel - draggingNode.data.catLevel + 1
+
+      if (type === 'inner') {
+        return (draggingNodeCount + dropNode.level) <= 3
+      }
+
+      // type == 'prev' 或 'next'，
+      return (draggingNodeCount + dropNode.parent.level) <= 3
+    },
+
+    // 计算当前正在被拖拽的节点的最大层级
+    calculateDraggingNodeMaxLevel (node) {
+      // 一定要加上这个，否则没有子节点的node进入不了下面的for循环，maxLevel会一直是初始值0
+      this.maxLevel = node.catLevel
+      if (node.children != null && node.children.length > 0) {
+        for (let i = 0; i < node.children.length; i++) {
+          if (node.children[i].catLevel > this.maxLevel) {
+            this.maxLevel = node.children[i].catLevel
+          }
+          this.calculateDraggingNodeMaxLevel(node.children[i])
+        }
+      }
+    },
+
+    submitData () {
+      if (this.dialogType === 'add') {
+        this.addCategory()
+      }
+
+      if (this.dialogType === 'edit') {
+        this.editCategory()
+      }
+    },
+
+    editCategory () {
+      let { catId, name, icon, productUnit } = this.category
+      this.$http({
+        url: this.$http.adornUrl('/product/category/update'),
+        method: 'post',
+        data: this.$http.adornData({ catId, name, icon, productUnit }, false)
+      }).then(({ data }) => {
+        this.$message({
+          message: '菜单修改成功',
+          type: 'success'
+        })
+        this.dialogVisible = false
+        this.getMenus()
+      })
+    },
+
     // 添加三级分类
     addCategory () {
       this.$http({
@@ -89,10 +172,39 @@ export default {
       })
     },
 
+    // 修改三级分类
+    edit (data) {
+      console.log('要修改的数据: ', data)
+      this.dialogVisible = true
+      this.dialogType = 'edit'
+      this.title = '修改分类'
+      this.$http({
+        url: this.$http.adornUrl(`/product/category/info/${data.catId}`),
+        method: 'get'
+      }).then(({ data }) => {
+        this.category.name = data.data.name
+        this.category.catId = data.data.catId
+        this.category.icon = data.data.icon
+        this.category.productUnit = data.data.productUnit
+        this.category.parentCid = data.data.parentCid
+        this.category.catLevel = data.data.catLevel
+        this.category.sort = data.data.sort
+        this.category.showStatus = data.data.showStatus
+      })
+    },
+
     append (data) {
       this.dialogVisible = true
       this.category.parentCid = data.catId
       this.category.catLevel = data.catLevel * 1 + 1
+      this.dialogType = 'add'
+      this.title = '添加分类'
+      this.category.catId = null
+      this.category.name = ''
+      this.category.icon = ''
+      this.category.productUnit = ''
+      this.category.sort = 0
+      this.category.showStatus = 1
     },
 
     remove (node, data) {
