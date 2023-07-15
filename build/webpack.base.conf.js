4cf56df1710b6b4d9182ecad53258c64d4541cc6
xyzgithome <2584118953@qq.com>
2023-07-15 19:48:09 +0800

    品牌logo上传及回显

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -40,7 +40,8 @@ module.exports = {
   },
   module: {
     rules: [
-      ...(config.dev.useEslint ? [createLintingRule()] : []),
+      // 关闭eslint检测代码格式
+      // ...(config.dev.useEslint ? [createLintingRule()] : []),
       {
         test: /\.vue$/,
         loader: 'vue-loader',
