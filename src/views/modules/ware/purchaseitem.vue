b101b0499cf23e404ede8db492ea49f05c3b4ea1
xyzgithome <2584118953@qq.com>
2023-08-07 23:12:41 +0800

    111

--- src/views/modules/ware/purchaseitem.vue
+++ src/views/modules/ware/purchaseitem.vue
@@ -1,13 +1,32 @@
 <template>
   <div class="mod-config">
-    <el-form :inline="true" :model="dataForm" @keyup.enter.native="getDataList()">
+    <el-form
+      :inline="true"
+      :model="dataForm"
+      @keyup.enter.native="getDataList()"
+    >
       <el-form-item label="仓库">
-        <el-select style="width:120px;" v-model="dataForm.wareId" placeholder="请选择仓库" clearable>
-          <el-option :label="w.name" :value="w.id" v-for="w in wareList" :key="w.id"></el-option>
+        <el-select
+          style="width: 120px"
+          v-model="dataForm.wareId"
+          placeholder="请选择仓库"
+          clearable
+        >
+          <el-option
+            :label="w.name"
+            :value="w.id"
+            v-for="w in wareList"
+            :key="w.id"
+          ></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="状态">
-        <el-select style="width:120px;" v-model="dataForm.status" placeholder="请选择状态" clearable>
+        <el-select
+          style="width: 120px"
+          v-model="dataForm.status"
+          placeholder="请选择状态"
+          clearable
+        >
           <el-option label="新建" :value="0"></el-option>
           <el-option label="已分配" :value="1"></el-option>
           <el-option label="正在采购" :value="2"></el-option>
@@ -16,7 +35,12 @@
         </el-select>
       </el-form-item>
       <el-form-item label="关键字">
-        <el-input style="width:120px;" v-model="dataForm.key" placeholder="参数名" clearable></el-input>
+        <el-input
+          style="width: 120px"
+          v-model="dataForm.key"
+          placeholder="参数名"
+          clearable
+        ></el-input>
       </el-form-item>
       <el-form-item>
         <el-button @click="getDataList()">查询</el-button>
@@ -24,8 +48,12 @@
           v-if="isAuth('ware:purchasedetail:save')"
           type="primary"
           @click="addOrUpdateHandle()"
-        >新增</el-button>
-        <el-dropdown @command="handleBatchCommand" :disabled="dataListSelections.length <= 0">
+          >新增</el-button
+        >
+        <el-dropdown
+          @command="handleBatchCommand"
+          :disabled="dataListSelections.length <= 0"
+        >
           <el-button type="danger">
             批量操作
             <i class="el-icon-arrow-down el-icon--right"></i>
@@ -42,28 +70,84 @@
       border
       v-loading="dataListLoading"
       @selection-change="selectionChangeHandle"
-      style="width: 100%;"
+      style="width: 100%"
     >
-      <el-table-column type="selection" header-align="center" align="center" width="50"></el-table-column>
-      <el-table-column prop="id" header-align="center" align="center" label="id"></el-table-column>
-      <el-table-column prop="purchaseId" header-align="center" align="center" label="采购单id"></el-table-column>
-      <el-table-column prop="skuId" header-align="center" align="center" label="采购商品id"></el-table-column>
-      <el-table-column prop="skuNum" header-align="center" align="center" label="采购数量"></el-table-column>
-      <el-table-column prop="skuPrice" header-align="center" align="center" label="采购金额"></el-table-column>
-      <el-table-column prop="wareId" header-align="center" align="center" label="仓库id"></el-table-column>
-      <el-table-column prop="status" header-align="center" align="center" label="状态">
+      <el-table-column
+        type="selection"
+        header-align="center"
+        align="center"
+        width="50"
+      ></el-table-column>
+      <el-table-column
+        prop="id"
+        header-align="center"
+        align="center"
+        label="id"
+      ></el-table-column>
+      <el-table-column
+        prop="purchaseId"
+        header-align="center"
+        align="center"
+        label="采购单id"
+      ></el-table-column>
+      <el-table-column
+        prop="skuId"
+        header-align="center"
+        align="center"
+        label="采购商品id"
+      ></el-table-column>
+      <el-table-column
+        prop="skuNum"
+        header-align="center"
+        align="center"
+        label="采购数量"
+      ></el-table-column>
+      <el-table-column
+        prop="skuPrice"
+        header-align="center"
+        align="center"
+        label="采购金额"
+      ></el-table-column>
+      <el-table-column
+        prop="wareId"
+        header-align="center"
+        align="center"
+        label="仓库id"
+      ></el-table-column>
+      <el-table-column
+        prop="status"
+        header-align="center"
+        align="center"
+        label="状态"
+      >
         <template slot-scope="scope">
-          <el-tag v-if="scope.row.status==0">新建</el-tag>
-          <el-tag type="info" v-if="scope.row.status==1">已分配</el-tag>
-          <el-tag type="wanring" v-if="scope.row.status==2">正在采购</el-tag>
-          <el-tag type="success" v-if="scope.row.status==3">已完成</el-tag>
-          <el-tag type="danger" v-if="scope.row.status==4">采购失败</el-tag>
+          <el-tag v-if="scope.row.status == 0">新建</el-tag>
+          <el-tag type="info" v-if="scope.row.status == 1">已分配</el-tag>
+          <el-tag type="wanring" v-if="scope.row.status == 2">正在采购</el-tag>
+          <el-tag type="success" v-if="scope.row.status == 3">已完成</el-tag>
+          <el-tag type="danger" v-if="scope.row.status == 4">采购失败</el-tag>
         </template>
       </el-table-column>
-      <el-table-column fixed="right" header-align="center" align="center" width="150" label="操作">
+      <el-table-column
+        fixed="right"
+        header-align="center"
+        align="center"
+        width="150"
+        label="操作"
+      >
         <template slot-scope="scope">
-          <el-button type="text" size="small" @click="addOrUpdateHandle(scope.row.id)">修改</el-button>
-          <el-button type="text" size="small" @click="deleteHandle(scope.row.id)">删除</el-button>
+          <el-button
+            type="text"
+            size="small"
+            @click="addOrUpdateHandle(scope.row.id)"
+            >修改</el-button
+          >
+          <el-button
+            type="text"
+            size="small"
+            @click="deleteHandle(scope.row.id)"
+            >删除</el-button
+          >
         </template>
       </el-table-column>
     </el-table>
@@ -77,7 +161,11 @@
       layout="total, sizes, prev, pager, next, jumper"
     ></el-pagination>
     <!-- 弹窗, 新增 / 修改 -->
-    <add-or-update v-if="addOrUpdateVisible" ref="addOrUpdate" @refreshDataList="getDataList"></add-or-update>
+    <add-or-update
+      v-if="addOrUpdateVisible"
+      ref="addOrUpdate"
+      @refreshDataList="getDataList"
+    ></add-or-update>
     <el-dialog title="合并到整单" :visible.sync="mergedialogVisible">
       <!-- id  assignee_id  assignee_name  phone   priority status -->
       <el-select v-model="purchaseId" placeholder="请选择" clearable filterable>
@@ -88,9 +176,9 @@
           :value="item.id"
         >
           <span style="float: left">{{ item.id }}</span>
-          <span
-            style="float: right; color: #8492a6; font-size: 13px"
-          >{{ item.assigneeName }}：{{item.phone}}</span>
+          <span style="float: right; color: #8492a6; font-size: 13px"
+            >{{ item.assigneeName }}：{{ item.phone }}</span
+          >
         </el-option>
       </el-select>
       <span slot="footer" class="dialog-footer">
@@ -104,7 +192,7 @@
 <script>
 import AddOrUpdate from "./purchasedetail-add-or-update";
 export default {
-  data() {
+  data () {
     return {
       dataForm: {
         key: "",
@@ -127,12 +215,12 @@ export default {
   components: {
     AddOrUpdate
   },
-  activated() {
+  activated () {
     this.getDataList();
     this.getWares();
   },
   methods: {
-    mergeItem() {
+    mergeItem () {
       let items = this.dataListSelections.map(item => {
         return item.id;
       });
@@ -152,25 +240,30 @@ export default {
               method: "post",
               data: this.$http.adornData({ items: items }, false)
             }).then(({ data }) => {
+              this.$message({
+                message: "合并整单成功",
+                type: "success"
+              });
               this.getDataList();
             });
           })
-          .catch(() => {});
+          .catch(() => { });
       } else {
         this.$http({
           url: this.$http.adornUrl("/ware/purchase/merge"),
           method: "post",
-          data: this.$http.adornData(
-            { purchaseId: this.purchaseId, items: items },
-            false
-          )
+          data: this.$http.adornData({ purchaseId: this.purchaseId, items: items }, false)
         }).then(({ data }) => {
+          this.$message({
+            message: "合并整单成功",
+            type: "success"
+          });
           this.getDataList();
         });
       }
       this.mergedialogVisible = false;
     },
-    getUnreceivedPurchase() {
+    getUnreceivedPurchase () {
       this.$http({
         url: this.$http.adornUrl("/ware/purchase/unreceive/list"),
         method: "get",
@@ -179,7 +272,7 @@ export default {
         this.purchasetableData = data.page.list;
       });
     },
-    handleBatchCommand(cmd) {
+    handleBatchCommand (cmd) {
       if (cmd == "delete") {
         this.deleteHandle();
       }
@@ -190,12 +283,12 @@ export default {
         } else {
           this.$alert("请先选择需要合并的需求", "提示", {
             confirmButtonText: "确定",
-            callback: action => {}
+            callback: action => { }
           });
         }
       }
     },
-    getWares() {
+    getWares () {
       this.$http({
         url: this.$http.adornUrl("/ware/wareinfo/list"),
         method: "get",
@@ -208,7 +301,7 @@ export default {
       });
     },
     // 获取数据列表
-    getDataList() {
+    getDataList () {
       this.dataListLoading = true;
       this.$http({
         url: this.$http.adornUrl("/ware/purchasedetail/list"),
@@ -232,34 +325,34 @@ export default {
       });
     },
     // 每页数
-    sizeChangeHandle(val) {
+    sizeChangeHandle (val) {
       this.pageSize = val;
       this.pageIndex = 1;
       this.getDataList();
     },
     // 当前页
-    currentChangeHandle(val) {
+    currentChangeHandle (val) {
       this.pageIndex = val;
       this.getDataList();
     },
     // 多选
-    selectionChangeHandle(val) {
+    selectionChangeHandle (val) {
       this.dataListSelections = val;
     },
     // 新增 / 修改
-    addOrUpdateHandle(id) {
+    addOrUpdateHandle (id) {
       this.addOrUpdateVisible = true;
       this.$nextTick(() => {
         this.$refs.addOrUpdate.init(id);
       });
     },
     // 删除
-    deleteHandle(id) {
+    deleteHandle (id) {
       var ids = id
         ? [id]
         : this.dataListSelections.map(item => {
-            return item.id;
-          });
+          return item.id;
+        });
       this.$confirm(
         `确定对[id=${ids.join(",")}]进行[${id ? "删除" : "批量删除"}]操作?`,
         "提示",
